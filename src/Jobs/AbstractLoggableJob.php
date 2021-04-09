c408af530d0363d83dbb473114df42a092820592
Steve Boyd <emteknetnz@gmail.com>
2021-04-09 21:35:07 +1200

    Queue next job via a matrix

--- src/Jobs/AbstractLoggableJob.php
+++ src/Jobs/AbstractLoggableJob.php
@@ -6,7 +6,6 @@ use Exception;
 use emteknetnz\DataFetcher\Misc\Logger;
 use SilverStripe\Core\Injector\Injectable;
 use SilverStripe\Core\Injector\Injector;
-use SilverStripe\ORM\FieldType\DBDatetime;
 use Symbiote\QueuedJobs\DataObjects\QueuedJobDescriptor;
 use Symbiote\QueuedJobs\Services\AbstractQueuedJob;
 use Symbiote\QueuedJobs\Services\QueuedJob;
@@ -52,14 +51,79 @@ abstract class AbstractLoggableJob extends AbstractQueuedJob
 
     abstract protected function processWithLogging(): void;
 
-    abstract protected function getSecondsBetweenJobs(): int;
+    abstract protected function getTimeMatrix(): array;
+
+    // ensure matrix has all days, and that it's in order
+    private function cleanMatrix(array $matrix) {
+        $m = [
+            'mon' => [],
+            'tue' => [],
+            'wed' => [],
+            'thu' => [],
+            'fri' => [],
+            'sat' => [],
+            'sun' => [],
+        ];
+        foreach ($matrix as $day => $times) {
+            $m[$day] = $times;
+        }
+        return $m;
+    }
+
+    private function deriveNextTime(array $matrix): string
+    {
+        $matrix = $this->cleanMatrix($matrix);
+
+        $today = strtolower(date('D')); // tue
+        $timeNow = (int) date('Gi'); // 730
+
+        // get all times after time now
+        $times = array_filter($matrix[$today], function($matrixTime) use ($timeNow) {
+            return $timeNow < $matrixTime;
+        });
+        if (!empty($times)) {
+            $mysqlDate = strtolower(date('Y-m-d'));
+            // reset index
+            $times = array_values($times);
+            return $this->buildMysqlDateTime($mysqlDate, $times[0]);
+        }
+        // find the next day with matrix entries
+        $days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+        $daystwice = array_merge($days, $days);
+        $foundToday = false;
+        $d = 1;
+        foreach ($daystwice as $day) {
+            if (!$foundToday) {
+                if ($day != $today) {
+                    continue;
+                }
+                $foundToday = true;
+                continue;
+            }
+            $mysqlDate = date('Y-m-d', strtotime("+{$d} day"));
+            $times = $matrix[$day];
+            $d++;
+            if (empty($times)) {
+                continue;
+            }
+            return $this->buildMysqlDateTime($mysqlDate, $times[0]);
+        }
+    }
+
+    private function buildMysqlDateTime($mysqlDate, $time)
+    {
+        preg_match('#^([0-9]{1,2})([0-9]{2})$#', $time, $m);
+        $mysqlTime = str_pad($m[1], 2, '0', STR_PAD_LEFT) . ':' . $m[2] . ':00';
+        return $mysqlDate . ' ' . $mysqlTime;
+    }
 
     private function queueNextJob(): void
     {
-        $timestamp = time() + $this->getSecondsBetweenJobs();
+        $matrix = $this->getTimeMatrix();
+        $nextTime = $this->deriveNextTime($matrix);
         QueuedJobService::singleton()->queueJob(
             Injector::inst()->create(static::class),
-            DBDatetime::create()->setValue($timestamp)->Rfc2822()
+            $nextTime
         );
     }
 }
